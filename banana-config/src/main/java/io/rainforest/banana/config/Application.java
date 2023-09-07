f403d2617aba9b6aec543dd3e78d91b749e432cc
落叶微风 <zeark@qq.com>
2023-09-07 10:07:10 +0800

    增加scc

---
+++ banana-config/src/main/java/io/rainforest/banana/config/Application.java
@@ -0,0 +1,16 @@
+package io.rainforest.banana.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
