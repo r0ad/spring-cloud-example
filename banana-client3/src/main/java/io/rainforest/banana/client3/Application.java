bd7410ee736e72690204366db3dc10ceea0eb1c5
sunz <zeark@qq.com>
2023-10-31 11:43:48 +0800

    fix some comment

--- banana-client3/src/main/java/io/rainforest/banana/client3/Application.java
+++ banana-client3/src/main/java/io/rainforest/banana/client3/Application.java
@@ -1,7 +1,6 @@
 package io.rainforest.banana.client3;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
