6f44b14eb840b6797f8edf73ff7e42da120d8721
sunz <zeark@qq.com>
2023-11-20 15:16:18 +0800

    修复ignore

--- banana-gateway-sso/src/test/java/io/rainforest/banana/gateway/sso/web/base/LoginTest.java
+++ banana-gateway-sso/src/test/java/io/rainforest/banana/gateway/sso/web/base/LoginTest.java
@@ -1,17 +1,11 @@
 package io.rainforest.banana.gateway.sso.web.base;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,6 +20,17 @@ public class LoginTest {
                 .exchange() // 发送GET请求并获取响应
                 .expectStatus().isOk() // 断言响应状态码为200
                 .expectBody().jsonPath("$.code").isEqualTo(200); // 断言响应体中的jsonPath("$.code")是否等于200
+    }
+
+    @Test
+    void testLoginFailure(@Autowired WebTestClient webClient) {
+        // 使用@Autowired注解获取WebTestClient对象，用于发送HTTP请求
+        webClient
+                .get().uri(url -> url.path("/user/login").queryParam("name", "test233").queryParam("pwd", "123456").build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange() // 发送GET请求并获取响应
+                .expectStatus().isOk() // 断言响应状态码为200
+                .expectBody().jsonPath("$.code").isEqualTo(500); // 断言响应体中的jsonPath("$.code")是否等于200
     }
 
 }
