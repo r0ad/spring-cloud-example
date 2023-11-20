420ea317ce9fa9898e2a4e42a885734855b5821e
sunz <zeark@qq.com>
2023-11-20 11:13:51 +0800

    增加sso单元测试

---
+++ banana-gateway-sso/src/test/java/io/rainforest/banana/gateway/sso/web/base/LoginTest.java
@@ -0,0 +1,31 @@
+package io.rainforest.banana.gateway.sso.web.base;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+
+    @Test
+    void testLoginSuccess(@Autowired WebTestClient webClient) {
+        // 使用@Autowired注解获取WebTestClient对象，用于发送HTTP请求
+        webClient
+                .get().uri(url -> url.path("/user/login").queryParam("name", "test").queryParam("pwd", "123456").build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange() // 发送GET请求并获取响应
+                .expectStatus().isOk() // 断言响应状态码为200
+                .expectBody().jsonPath("$.code").isEqualTo(200); // 断言响应体中的jsonPath("$.code")是否等于200
+    }
+
+}
